5f5cd4f77747051980c02fa7438ca34b0874ea48
Kumar Shridhar <shridhar.stark@gmail.com>
2018-08-17 14:16:15 +0200

    Update resnet.py

--- utils/NonBayesianModels/resnet.py
+++ utils/NonBayesianModels/resnet.py
@@ -11,8 +11,8 @@ def conv3x3(in_planes, out_planes, stride=1):
 def conv_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
-        init.xavier_uniform(m.weight, gain=np.sqrt(2))
-        init.constant(m.bias, 0)
+        nn.init.xavier_uniform(m.weight, gain=np.sqrt(2))
+        nn.init.constant(m.bias, 0)
 
 def cfg(depth):
     depth_lst = [18, 34, 50, 101, 152]
