217302dc3e5ab1aab11acdd1f7152038a1176c6b
kumar-shridhar <shridhar.stark@gmail.com>
2018-09-01 16:34:44 +0200

    added graphs for cifar 10 and cifar 100

--- extraction/extract_acc.py
+++ extraction/extract_acc.py
@@ -9,10 +9,10 @@ plt.rc('font', family='serif', size=32)
 plt.rcParams.update({'xtick.labelsize': 32, 'ytick.labelsize': 32, 'axes.labelsize': 32})
 
 # change for given number of tasks
-os.chdir("/home/felix/Dropbox/Research/publications/Bayesian_CNN/results/")
+os.chdir("../results/")
 
-with open("diagnostics_MNIST.txt", 'r') as file:
-    acc = re.findall(r"'acc':\s+tensor\((.*?)\)", file.read())
+with open("diagnostics_NonBayeslenet_cifar10.txt", 'r') as file:
+    acc = re.findall(r"'Accuracy':\s+tensor\((.*?)\)", file.read())
 print(acc)
 
 train_1 = acc[0::2]
@@ -21,8 +21,8 @@ valid_1 = acc[1::2]
 train_1 = np.array(train_1).astype(np.float32)
 valid_1 = np.array(valid_1).astype(np.float32)
 
-with open("diagnostics_CIFAR-10.txt", 'r') as file:
-    acc = re.findall(r"'acc':\s+tensor\((.*?)\)", file.read())
+with open("diagnostics_NonBayeslenet_cifar100.txt", 'r') as file:
+    acc = re.findall(r"'Accuracy':\s+tensor\((.*?)\)", file.read())
 print(acc)
 
 train_2 = acc[0::2]
@@ -31,8 +31,8 @@ valid_2 = acc[1::2]
 train_2 = np.array(train_2).astype(np.float32)
 valid_2 = np.array(valid_2).astype(np.float32)
 
-with open("diagnostics_BBBMNIST.txt", 'r') as file:
-    acc = re.findall(r"'acc':\s+tensor\((.*?)\)", file.read())
+with open("diagnostics_Bayeslenet_cifar10.txt", 'r') as file:
+    acc = re.findall(r"'Accuracy':\s+tensor\((.*?)\)", file.read())
 print(acc)
 
 train_3 = acc[0::2]
@@ -41,8 +41,8 @@ valid_3 = acc[1::2]
 train_3 = np.array(train_3).astype(np.float32)
 valid_3 = np.array(valid_3).astype(np.float32)
 
-with open("diagnostics_BBBCIFAR-10.txt", 'r') as file:
-    acc = re.findall(r"'acc':\s+tensor\((.*?)\)", file.read())
+with open("diagnostics_Bayeslenet_cifar100.txt", 'r') as file:
+    acc = re.findall(r"'Accuracy':\s+tensor\((.*?)\)", file.read())
 print(acc)
 
 train_4 = acc[0::2]
@@ -60,12 +60,12 @@ print(valid_2)
 print(valid_3)
 print(valid_4)
 
-plt.plot(valid_1, label=r"MNIST, $MLE$", color='maroon', linestyle='--')
-plt.plot(valid_2, label=r"CIFAR-10, $MLE$", color='darkblue', linestyle='--')
-plt.plot(valid_3, label=r"MNIST, prior: $U(a, b)$", color='maroon')
-plt.plot(valid_4, label=r"CIFAR-10, prior: $U(a, b)$", color='darkblue')
-plt.plot(valid_5, label=r"Fashion-MNIST, prior: $U(a, b)$", color='m')
-plt.plot(valid_6, label=r"Fashion-MNIST, $MLE$", color='m', linestyle='--')
+plt.plot(valid_1, label=r"CIFAR-10, $MLE$", color='maroon', linestyle='--')
+plt.plot(valid_2, label=r"CIFAR-100, $MLE$", color='darkblue', linestyle='--')
+plt.plot(valid_3, label=r"CIFAR-10, prior: $U(a, b)$", color='maroon')
+plt.plot(valid_4, label=r"CIFAR-100, prior: $U(a, b)$", color='darkblue')
+#plt.plot(valid_5, label=r"Fashion-MNIST, prior: $U(a, b)$", color='m')
+#plt.plot(valid_6, label=r"Fashion-MNIST, $MLE$", color='m', linestyle='--')
 
 
 
