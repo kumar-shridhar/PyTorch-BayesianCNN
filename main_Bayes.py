a1f912a06b4791ea5a38550a585c4dbd1c19f1f3
kumar-shridhar <shridhar.stark@gmail.com>
2018-09-12 00:49:20 +0200

    comments are of no use. Cleaner and nicer

--- main_Bayes.py
+++ main_Bayes.py
@@ -30,15 +30,7 @@ from utils.BayesianModels.BayesianSqueezeNet import BBBSqueezeNet
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR-10 Training')
-#parser.add_argument('--lr', default=0.001, type=float, help='learning_rate')
 parser.add_argument('--net_type', default='alexnet', type=str, help='model')
-#parser.add_argument('--depth', default=28, type=int, help='depth of model')
-#parser.add_argument('--widen_factor', default=10, type=int, help='width of model')
-#parser.add_argument('--num_samples', default=10, type=int, help='Number of samples')
-#parser.add_argument('--beta_type', default="Blundell", type=str, help='Beta type')
-#parser.add_argument('--p_logvar_init', default=0, type=int, help='p_logvar_init')
-#parser.add_argument('--q_logvar_init', default=-10, type=int, help='q_logvar_init')
-#parser.add_argument('--weight_decay', default=0.0005, type=float, help='weight_decay')
 parser.add_argument('--dataset', default='cifar100', type=str, help='dataset = [mnist/cifar10/cifar100/fashionmnist/stl10]')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
 parser.add_argument('--testOnly', '-t', action='store_true', help='Test mode with the saved model')
