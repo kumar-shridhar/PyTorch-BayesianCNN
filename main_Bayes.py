534373985af44eff2dfd4ef28b3073fde730a73b
kumar-shridhar <purumehta69@gmail.com>
2018-10-03 17:20:08 +0200

    Added some data augmentation flip and crop

--- main_Bayes.py
+++ main_Bayes.py
@@ -55,12 +55,16 @@ print('\n[Phase 1] : Data Preparation')
 
 transform_train = transforms.Compose([
     transforms.Resize((resize, resize)),
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize(cf.mean[args.dataset], cf.std[args.dataset]),
 ])  # meanstd transformation
 
 transform_test = transforms.Compose([
     transforms.Resize((resize, resize)),
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize(cf.mean[args.dataset], cf.std[args.dataset]),
 ])
