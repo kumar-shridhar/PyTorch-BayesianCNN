96052f01a2806a4053ea62e5a2cc58fd2f49f50e
Piyush Raikwar <piyushraikwar555@gmail.com>
2020-02-21 00:01:10 +0530

    Update models

--- models/BayesianModels/BayesianAlexNet.py
+++ models/BayesianModels/BayesianAlexNet.py
@@ -13,23 +13,23 @@ class BBBAlexNet(ModuleWrapper):
 
         self.num_classes = outputs
 
-        self.conv1 = BBBConv2d(inputs, 64, 11, alpha_shape=(1,1), stride=4, padding=5, bias=False)
+        self.conv1 = BBBConv2d(inputs, 64, 11, alpha_shape=(1,1), stride=4, padding=5, bias=False, name='conv1')
         self.soft1 = nn.Softplus()
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.conv2 = BBBConv2d(64, 192, 5, alpha_shape=(1,1), padding=2, bias=False)
+        self.conv2 = BBBConv2d(64, 192, 5, alpha_shape=(1,1), padding=2, bias=False, name='conv2')
         self.soft2 = nn.Softplus()
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.conv3 = BBBConv2d(192, 384, 3, alpha_shape=(1,1), padding=1, bias=False)
+        self.conv3 = BBBConv2d(192, 384, 3, alpha_shape=(1,1), padding=1, bias=False, name='conv3')
         self.soft3 = nn.Softplus()
 
-        self.conv4 = BBBConv2d(384, 256, 3, alpha_shape=(1,1), padding=1, bias=False)
+        self.conv4 = BBBConv2d(384, 256, 3, alpha_shape=(1,1), padding=1, bias=False, name='conv4')
         self.soft4 = nn.Softplus()
 
-        self.conv5 = BBBConv2d(256, 128, 3, alpha_shape=(1,1), padding=1, bias=False)
+        self.conv5 = BBBConv2d(256, 128, 3, alpha_shape=(1,1), padding=1, bias=False, name='conv5')
         self.soft5 = nn.Softplus()
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.flatten = FlattenLayer(1 * 1 * 128)
-        self.classifier = BBBLinear(1 * 1 * 128, outputs, alpha_shape=(1,1), bias=False)
+        self.classifier = BBBLinear(1 * 1 * 128, outputs, alpha_shape=(1,1), bias=False, name='classifier')
