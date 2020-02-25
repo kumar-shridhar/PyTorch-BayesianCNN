4c0bb6cb3e25ade7a3fc033b926a2a32544c2d96
Piyush Raikwar <piyushraikwar555@gmail.com>
2020-02-25 20:56:30 +0530

    Add save gif for distplot

--- visualize_mean_var.py
+++ visualize_mean_var.py
@@ -1,4 +1,5 @@
 import os
+import imageio
 import argparse
 import numpy as np
 import seaborn as sns
@@ -12,7 +13,7 @@ def draw_distributions(filename, save_dir, type='mean', node_no=0, save_plots=Fa
     layer = file_desc['layer_name']
     means, std = utils.load_mean_std_from_file(filename)
     data = means if type=='mean' else stds
-
+    frames = []
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
@@ -26,13 +27,16 @@ def draw_distributions(filename, save_dir, type='mean', node_no=0, save_plots=Fa
         plt.title(f'Distribution for {layer} node {node_no}: Epoch-{epoch} Iteration-{iteration}')
         plt.xlabel(f'Value of {type}')
         plt.ylabel('Density')
-        plt.show(block=False)
+        fig.canvas.draw()
+        if save_plots:
+            frame = np.array(fig.canvas.renderer.buffer_rgba())
+            frames.append(frame)
         plt.pause(plot_time)
         ax.clear()
     plt.close()
 
     if save_plots:
-        raise NotImplementedError
+        imageio.mimsave(save_dir + f'{layer}-node_{node_no}-{type}-distplot.gif', frames, fps=1/plot_time)
 
 
 def draw_lineplot(filename, save_dir, type='mean', node_no=0, save_plots=False, plot_time=5):
@@ -54,7 +58,7 @@ def draw_lineplot(filename, save_dir, type='mean', node_no=0, save_plots=False, 
     plt.show(block=False)
     plt.pause(plot_time)
     if save_plots:
-        plt.savefig(save_dir + 'lineplot.jpg')
+        plt.savefig(save_dir + f'{layer}-node_{node_no}-{type}-lineplot.jpg')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = "Visualize Mean and Variance")
@@ -62,11 +66,12 @@ if __name__ == '__main__':
     parser.add_argument('--data_type', default='mean', type=str, help='Draw plots for what? mean or std?')
     parser.add_argument('--node_no', default=0, type=int, help='Draw plots for which node?')
     parser.add_argument('--plot_type', default='lineplot', type=str, help='Which plot to draw? lineplot or distplot?')
-    parser.add_argument('--plot_time', default=1, type=int, help='Pause the plot for how much time?')
+    parser.add_argument('--plot_time', default=1, type=float, help='Pause the plot for how much time?')
     parser.add_argument('--save_plots', default=0, type=int, help='Save plots? 0 (No) or 1 (Yes)')
     parser.add_argument('--save_dir', default='', type=str, help='Save plots to which directory?(End with a /)')
     args = parser.parse_args()
 
+    save_dir = None
     if args.save_plots:
         save_dir = None if args.save_dir=='' else args.save_dir
         if not save_dir:
