9d7bd9ae53264faa46bfa6bc9ea58ff38bc8577e
Piyush Raikwar <piyushraikwar555@gmail.com>
2020-03-06 20:08:20 +0530

    Add 5 task 2 classes each dataset

--- data/data.py
+++ data/data.py
@@ -72,7 +72,7 @@ def getDataset(dataset):
         num_classes = 10
         inputs = 1
 
-    elif(dataset == 'SplitMNIST-1'):
+    elif(dataset == 'SplitMNIST-2.1'):
         trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform_mnist)
         testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform_mnist)
 
@@ -84,7 +84,7 @@ def getDataset(dataset):
         num_classes = 5
         inputs = 1
 
-    elif(dataset == 'SplitMNIST-2'):
+    elif(dataset == 'SplitMNIST-2.2'):
         trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform_mnist)
         testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform_mnist)
 
@@ -96,6 +96,74 @@ def getDataset(dataset):
         trainset = CustomDataset(train_data, train_targets, transform=transform_split_mnist)
         testset = CustomDataset(test_data, test_targets, transform=transform_split_mnist)
         num_classes = 5
+        inputs = 1
+
+    elif(dataset == 'SplitMNIST-5.1'):
+        trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform_mnist)
+        testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform_mnist)
+
+        train_data, train_targets = extract_classes(trainset, [0, 1])
+        test_data, test_targets = extract_classes(testset, [0, 1])
+
+        trainset = CustomDataset(train_data, train_targets, transform=transform_split_mnist)
+        testset = CustomDataset(test_data, test_targets, transform=transform_split_mnist)
+        num_classes = 2
+        inputs = 1
+
+    elif(dataset == 'SplitMNIST-5.2'):
+        trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform_mnist)
+        testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform_mnist)
+
+        train_data, train_targets = extract_classes(trainset, [2, 3])
+        test_data, test_targets = extract_classes(testset, [2, 3])
+        train_targets -= 2 # Mapping target 2-3 to 0-1
+        test_targets -= 2 # Hence, add 2 after prediction
+
+        trainset = CustomDataset(train_data, train_targets, transform=transform_split_mnist)
+        testset = CustomDataset(test_data, test_targets, transform=transform_split_mnist)
+        num_classes = 2
+        inputs = 1
+
+    elif(dataset == 'SplitMNIST-5.3'):
+        trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform_mnist)
+        testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform_mnist)
+
+        train_data, train_targets = extract_classes(trainset, [4, 5])
+        test_data, test_targets = extract_classes(testset, [4, 5])
+        train_targets -= 4 # Mapping target 4-5 to 0-1
+        test_targets -= 4 # Hence, add 4 after prediction
+
+        trainset = CustomDataset(train_data, train_targets, transform=transform_split_mnist)
+        testset = CustomDataset(test_data, test_targets, transform=transform_split_mnist)
+        num_classes = 2
+        inputs = 1
+
+    elif(dataset == 'SplitMNIST-5.4'):
+        trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform_mnist)
+        testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform_mnist)
+
+        train_data, train_targets = extract_classes(trainset, [6, 7])
+        test_data, test_targets = extract_classes(testset, [6, 7])
+        train_targets -= 6 # Mapping target 6-7 to 0-1
+        test_targets -= 6 # Hence, add 6 after prediction
+
+        trainset = CustomDataset(train_data, train_targets, transform=transform_split_mnist)
+        testset = CustomDataset(test_data, test_targets, transform=transform_split_mnist)
+        num_classes = 2
+        inputs = 1
+
+    elif(dataset == 'SplitMNIST-5.5'):
+        trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform_mnist)
+        testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform_mnist)
+
+        train_data, train_targets = extract_classes(trainset, [8, 9])
+        test_data, test_targets = extract_classes(testset, [8, 9])
+        train_targets -= 8 # Mapping target 8-9 to 0-1
+        test_targets -= 8 # Hence, add 8 after prediction
+
+        trainset = CustomDataset(train_data, train_targets, transform=transform_split_mnist)
+        testset = CustomDataset(test_data, test_targets, transform=transform_split_mnist)
+        num_classes = 2
         inputs = 1
 
     return trainset, testset, inputs, num_classes
