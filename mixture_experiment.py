2903dc229b0fa58e72e85f5e1154368c3f3d2d86
Piyush Raikwar <piyushraikwar555@gmail.com>
2020-03-31 19:21:45 +0530

    Add Logger

--- mixture_experiment.py
+++ mixture_experiment.py
@@ -1,9 +1,55 @@
+import os
+import sys
+import datetime
 import torch
+import contextlib
 
 from utils_mixture import *
 from layers.BBBLinear import BBBLinear
 
 
+@contextlib.contextmanager
+def print_to_logfile(file):
+    # capture all outputs to a log file while still printing it
+    class Logger:
+        def __init__(self, file):
+            self.terminal = sys.stdout
+            self.log = file
+
+        def write(self, message):
+            self.terminal.write(message)
+            self.log.write(message)
+
+        def __getattr__(self, attr):
+            return getattr(self.terminal, attr)
+
+    logger = Logger(file)
+
+    _stdout = sys.stdout
+    sys.stdout = logger
+    try:
+        yield logger.log
+    finally:
+        sys.stdout = _stdout
+
+
+def initiate_experiment(experiment):
+
+    def decorator():
+        log_file_dir = "experiments/mixtures/"
+        log_file = log_file_dir + experiment.__name__ + ".txt"
+        if not os.path.exists(log_file):
+            os.makedirs(log_file_dir, exist_ok=True)
+        with print_to_logfile(open(log_file, 'a')):
+            print("Performing experiment:", experiment.__name__)
+            print("Date-Time:", datetime.datetime.now())
+            print("\n", end="")
+            experiment()
+            print("\n\n", end="")
+    return decorator
+
+
+@initiate_experiment
 def experiment_average_weights_mixture_model():
     num_tasks = 2
     weights_dir = "checkpoints/MNIST/bayesian/splitted/2-tasks/"
@@ -23,6 +69,7 @@ def experiment_average_weights_mixture_model():
     print("Model-Mix, Loader-2:", calculate_accuracy(net_mix, loaders2[1]))
 
 
+@initiate_experiment
 def experiment_simultaneous_average_weights_mixture_model_with_uncertainty():
     num_tasks = 2
     weights_dir = "checkpoints/MNIST/bayesian/splitted/2-tasks/"
@@ -53,6 +100,7 @@ def experiment_simultaneous_average_weights_mixture_model_with_uncertainty():
     print("Model-Mix, Loader-2:", predict_using_epistemic_uncertainty_with_mixture_model(net_mix, fc3_1, fc3_2, loaders2[1]))
 
 
+@initiate_experiment
 def experiment_simultaneous_without_mixture_model_with_uncertainty():
     num_tasks = 2
     weights_dir = "checkpoints/MNIST/bayesian/splitted/2-tasks/"
@@ -68,6 +116,7 @@ def experiment_simultaneous_without_mixture_model_with_uncertainty():
     print("Both Models, Loader-2:", predict_using_epistemic_uncertainty_without_mixture_model(net1, net2, loaders2[1]))
 
 
+@initiate_experiment
 def experiment_simple_bayesian_model_with_uncertainty():
     num_tasks = 2
     weights_dir = "checkpoints/MNIST/bayesian/splitted/2-tasks/"
