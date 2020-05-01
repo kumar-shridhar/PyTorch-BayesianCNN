7e2ebe8a5d86320ea2365c40a73c55aece1136b8
Piyush Raikwar <piyushraikwar555@gmail.com>
2020-05-01 15:22:07 +0530

    'Merge' bayesian models from bbb_lrt

--- models/BayesianModels/BayesianLeNet.py
+++ models/BayesianModels/BayesianLeNet.py
@@ -13,19 +13,19 @@ class BBBLeNet(ModuleWrapper):
 
         self.num_classes = outputs
 
-        self.conv1 = BBBConv2d(inputs, 6, 5, alpha_shape=(1,1), padding=0, bias=False, name='conv1')
+        self.conv1 = BBBConv2d(inputs, 6, 5, padding=0, bias=True)
         self.soft1 = nn.Softplus()
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.conv2 = BBBConv2d(6, 16, 5, alpha_shape=(1,1), padding=0, bias=False, name='conv2')
+        self.conv2 = BBBConv2d(6, 16, 5, padding=0, bias=True)
         self.soft2 = nn.Softplus()
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.flatten = FlattenLayer(5 * 5 * 16)
-        self.fc1 = BBBLinear(5 * 5 * 16, 120, alpha_shape=(1,1), bias=False, name='fc1')
+        self.fc1 = BBBLinear(5 * 5 * 16, 120, bias=True)
         self.soft3 = nn.Softplus()
 
-        self.fc2 = BBBLinear(120, 84, alpha_shape=(1,1), bias=False, name='fc2')
+        self.fc2 = BBBLinear(120, 84, bias=True)
         self.soft4 = nn.Softplus()
 
-        self.fc3 = BBBLinear(84, outputs, alpha_shape=(1,1), bias=False, name='fc3')
+        self.fc3 = BBBLinear(84, outputs, bias=True)
