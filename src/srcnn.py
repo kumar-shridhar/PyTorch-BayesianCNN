0d2e984ad91c7019132a4c9231439dbe33fa6540
fedorenkood <fedorenkoalex23@gmail.com>
2023-04-07 20:44:58 -0400

    image super-resolution

---
+++ src/srcnn.py
@@ -0,0 +1,29 @@
+import torch.nn as nn
+import torch.nn.functional as F
+class SRCNN(nn.Module):
+    def __init__(self):
+        super(SRCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=9, padding=2, padding_mode='replicate') # padding mode same as original Caffe code
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=1, padding=2, padding_mode='replicate')
+        self.conv3 = nn.Conv2d(32, 1, kernel_size=5, padding=2, padding_mode='replicate')
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.conv3(x)
+        return x
+    
+
+class Net(nn.Module):
+    def __init__(self, upscale_factor):
+        super(Net, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 64, (5, 5), (1, 1), (2, 2))
+        self.conv2 = nn.Conv2d(64, 32, (3, 3), (1, 1), (1, 1))
+        self.conv3 = nn.Conv2d(32, 1 * (upscale_factor ** 2), (3, 3), (1, 1), (1, 1))
+        self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
+
+    def forward(self, x):
+        x = F.tanh(self.conv1(x))
+        x = F.tanh(self.conv2(x))
+        x = F.sigmoid(self.pixel_shuffle(self.conv3(x)))
+        return x
---
+++ src/train.py
@@ -0,0 +1,178 @@
+import torch
+import matplotlib
+import matplotlib.pyplot as plt
+import time
+import h5py
+import srcnn
+import torch.optim as optim
+import torch.nn as nn
+import numpy as np
+import math
+
+from torch.utils.data import DataLoader, Dataset
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from torchvision.utils import save_image
+
+matplotlib.style.use('ggplot')
+
+# learning parameters
+batch_size = 64 # batch size, reduce if facing OOM error
+epochs = 20 # number of epochs to train the SRCNN model for
+lr = 0.001 # the learning rate
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# input image dimensions
+img_rows, img_cols = 33, 33
+out_rows, out_cols = 33, 33
+
+
+file = h5py.File('../input/train_mscale.h5')
+# `in_train` has shape (21884, 33, 33, 1) which corresponds to
+# 21884 image patches of 33 pixels height & width and 1 color channel
+in_train = file['data'][:] # the training data
+out_train = file['label'][:] # the training labels
+file.close()
+# change the values to float32
+in_train = in_train.astype('float32')
+out_train = out_train.astype('float32')
+
+(x_train, x_val, y_train, y_val) = train_test_split(in_train, out_train, test_size=0.25)
+print('Training samples: ', x_train.shape[0])
+print('Validation samples: ', x_val.shape[0])
+
+# the dataset module
+class SRCNNDataset(Dataset):
+    def __init__(self, image_data, labels):
+        self.image_data = image_data
+        self.labels = labels
+    def __len__(self):
+        return (len(self.image_data))
+    def __getitem__(self, index):
+        image = self.image_data[index]
+        label = self.labels[index]
+        return (
+            torch.tensor(image, dtype=torch.float),
+            torch.tensor(label, dtype=torch.float)
+        )
+    
+# train and validation data
+train_data = SRCNNDataset(x_train, y_train)
+val_data = SRCNNDataset(x_val, y_val)
+# train and validation loaders
+train_loader = DataLoader(train_data, batch_size=batch_size)
+val_loader = DataLoader(val_data, batch_size=batch_size)
+
+
+# initialize the model
+print('Computation device: ', device)
+model = srcnn.SRCNN().to(device)
+print(model)
+
+# optimizer
+optimizer = optim.Adam(model.parameters(), lr=lr)
+# loss function 
+criterion = nn.MSELoss()
+
+def psnr(label, outputs, max_val=1.):
+    """
+    Compute Peak Signal to Noise Ratio (the higher the better).
+    PSNR = 20 * log10(MAXp) - 10 * log10(MSE).
+    https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio#Definition
+    First we need to convert torch tensors to NumPy operable.
+    """
+    label = label.cpu().detach().numpy()
+    outputs = outputs.cpu().detach().numpy()
+    img_diff = outputs - label
+    rmse = math.sqrt(np.mean((img_diff) ** 2))
+    if rmse == 0:
+        return 100
+    else:
+        PSNR = 20 * math.log10(max_val / rmse)
+        return PSNR
+    
+
+def train(model, dataloader):
+    model.train()
+    running_loss = 0.0
+    running_psnr = 0.0
+    for bi, data in tqdm(enumerate(dataloader), total=int(len(train_data)/dataloader.batch_size)):
+        image_data = data[0].to(device)
+        label = data[1].to(device)
+        
+        # zero grad the optimizer
+        optimizer.zero_grad()
+        outputs = model(image_data)
+        loss = criterion(outputs, label)
+        # backpropagation
+        loss.backward()
+        # update the parameters
+        optimizer.step()
+        # add loss of each item (total items in a batch = batch size)
+        running_loss += loss.item()
+        # calculate batch psnr (once every `batch_size` iterations)
+        batch_psnr =  psnr(label, outputs)
+        running_psnr += batch_psnr
+    final_loss = running_loss/len(dataloader.dataset)
+    final_psnr = running_psnr/int(len(train_data)/dataloader.batch_size)
+    return final_loss, final_psnr
+
+def validate(model, dataloader, epoch):
+    model.eval()
+    running_loss = 0.0
+    running_psnr = 0.0
+    with torch.no_grad():
+        for bi, data in tqdm(enumerate(dataloader), total=int(len(val_data)/dataloader.batch_size)):
+            image_data = data[0].to(device)
+            label = data[1].to(device)
+            
+            outputs = model(image_data)
+            loss = criterion(outputs, label)
+            # add loss of each item (total items in a batch = batch size) 
+            running_loss += loss.item()
+            # calculate batch psnr (once every `batch_size` iterations)
+            batch_psnr = psnr(label, outputs)
+            running_psnr += batch_psnr
+        outputs = outputs.cpu()
+        save_image(outputs, f"../outputs/val_sr{epoch}.png")
+    final_loss = running_loss/len(dataloader.dataset)
+    final_psnr = running_psnr/int(len(val_data)/dataloader.batch_size)
+    return final_loss, final_psnr
+
+train_loss, val_loss = [], []
+train_psnr, val_psnr = [], []
+start = time.time()
+for epoch in range(epochs):
+    print(f"Epoch {epoch + 1} of {epochs}")
+    train_epoch_loss, train_epoch_psnr = train(model, train_loader)
+    val_epoch_loss, val_epoch_psnr = validate(model, val_loader, epoch)
+    print(f"Train PSNR: {train_epoch_psnr:.3f}")
+    print(f"Val PSNR: {val_epoch_psnr:.3f}")
+    train_loss.append(train_epoch_loss)
+    train_psnr.append(train_epoch_psnr)
+    val_loss.append(val_epoch_loss)
+    val_psnr.append(val_epoch_psnr)
+end = time.time()
+print(f"Finished training in: {((end-start)/60):.3f} minutes")
+
+# loss plots
+plt.figure(figsize=(10, 7))
+plt.plot(train_loss, color='orange', label='train loss')
+plt.plot(val_loss, color='red', label='validataion loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig('../outputs/loss.png')
+plt.show()
+# psnr plots
+plt.figure(figsize=(10, 7))
+plt.plot(train_psnr, color='green', label='train PSNR dB')
+plt.plot(val_psnr, color='blue', label='validataion PSNR dB')
+plt.xlabel('Epochs')
+plt.ylabel('PSNR (dB)')
+plt.legend()
+plt.savefig('../outputs/psnr.png')
+plt.show()
+# save the model to disk
+print('Saving model...')
+torch.save(model.state_dict(), '../outputs/model.pth')
